27244383f69e3eb850e86199f7835d0d8eea8d2a
Masato Morita <m.morita44@hotmail.com>
2017-05-16 23:03:22 -0700

    Add datelist xml parser and execute ffmpeg

--- nhk-stream.py
+++ nhk-stream.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
+import traceback
+from argparse import ArgumentParser
 from enum import unique, Enum
-from optparse import make_option, OptionParser
+from os import makedirs
+from os.path import join, isdir
+from subprocess import check_call
+from xml.etree import ElementTree
 
+from requests import HTTPError
 from requests import Session
 
 
 @unique
-class Sound(Enum):
-    """NHK Gogaku sound files list."""
+class Music(Enum):
+    """NHK Gogaku music files list."""
     basic1 = 'english/basic1'
     basic2 = 'english/basic2'
     basic3 = 'english/basic3'
@@ -32,30 +38,50 @@ class Sound(Enum):
 if __name__ == '__main__':
 
     # Read command options
-    usage = "usage: %prog [OPTION...] [SOUND...]"
-    description = "'nhk-stream.py' downloads NHK Gogaku Sound files. " \
-                  + "SOUND: {}".format(",".join([sound.name for sound in Sound]))
-    options = [
-        make_option('-f', '--force', action="store_true", dest='force',
-                    help='overwrite files (default "%default")'),
-        make_option('-C', '--directory', dest='directory', default='',
-                    help='change directory (default "%default")'),
-    ]
-    options, args = OptionParser(usage, options, description=description).parse_args()
+    description = "'nhk-stream.py' downloads NHK Gogaku music files. "
+    parser = ArgumentParser(description=description)
+    parser.add_argument('music', type=str, nargs='+',
+                        help='music file name. choose [{}]'.format(",".join([member.name for member in Music])))
+    parser.add_argument('-f', '--force', action="store_true", help='overwrite files')
+    parser.add_argument('-C', '--directory', type=str, default='',
+                        help='change output directory (default: "%(default)s")')
+    args = parser.parse_args()
 
-    if len(args) == 0:
-        raise ValueError("nhk-stream.py: missing operand.\n"
-                         + "Try 'nhk-stream.py --help' or 'nhk-stream.py --usage' for more information")
+    for name in args.music:
+        if name not in [member.name for member in Music]:
+            parser.error("Invalid sound file name [{}]".format(name))
 
-    for arg in args:
-        if arg not in [sound.name for sound in Sound]:
-            raise ValueError("Invalid sound file.\n"
-                             + "Try 'nhk-stream.py --help' or 'nhk-stream.py --usage' for more information")
+    session = Session()
 
-    # sound files, path,
+    try:
+        # Get data list xml file
+        musics = dict()
+        for name in args.music:
+            resp = session.get("https://cgi2.nhk.or.jp/gogaku/st/xml/{}/listdataflv.xml".format(Music[name].value))
+            if resp.status_code != 200:
+                raise HTTPError("Invalid URL [{}]".format(resp.url), response=resp)
 
-    # Get data list xml file
+            root = ElementTree.fromstring(resp.content.decode())
+            musics[name] = [dict(title=e.get('title'), hdate=e.get('hdate'), kouza=e.get('kouza'), code=e.get('code'),
+                                 file=e.get('file'), nendo=e.get('nendo'), pgcode=e.get('pgcode')) for e in root.iter('music')]
 
-    # Execute ffmpeg
+        # Execute ffmpeg
+        for name, data_list in musics.items():
 
-    exit(0)
+            # Make music files directory
+            dir_name = join(args.directory, name)
+            if not isdir(dir_name):
+                makedirs(dir_name)
+
+            for data in data_list:
+                file = join(args.directory, name, "{}_{}_{}.mp3".format(data['title'], data['nendo'], data['hdate']))
+                if isdir(file) and args.force:
+                    continue
+                url = "https://nhk-vh.akamaihd.net/i/gogaku-stream/mp4/{}/master.m3u8".format(data['file'])
+                check_call(["ffmpeg", "-y", "-i", url, "-ab", "64k", "-id3v2_version", "3", file])
+
+        exit(0)
+
+    except Exception as e:
+        print(traceback.format_exc())
+        exit(1)
